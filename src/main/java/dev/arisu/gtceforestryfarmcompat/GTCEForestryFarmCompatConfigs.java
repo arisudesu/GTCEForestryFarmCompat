91cde785ec996af9a57a9f63ea6cbf4df80bf04b
Arisu Desu <arisudesu@yandex.ru>
2022-07-31 22:16:03 +0300

    Remove unnecessary mod config

--- src/main/java/dev/arisu/gtceforestryfarmcompat/GTCEForestryFarmCompatConfigs.java
+++
@@ -1,7 +0,0 @@
-package dev.arisu.gtceforestryfarmcompat;
-
-import net.minecraftforge.common.config.Config;
-
-@Config(modid = GTCEForestryFarmCompat.MODID)
-public class GTCEForestryFarmCompatConfigs {
-}
